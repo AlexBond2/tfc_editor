9b419b94793a7b6f5910963079b77fb3869c5bf0
Oscar Dahlqvist <oscdah@student.chalmers.se>
2022-05-06 23:36:30 +0200

    visual studio gui

--- src/Program.cs
+++ src/Program.cs
@@ -15,14 +15,7 @@ namespace PaladinsTfc
     }
 
     private static void gui(){
-      Form mainForm = new Form();
-      Label lblFirst = new Label();
-      mainForm.Width = 300;
-      mainForm.Height = 400; 
-      lblFirst.Text = "Hello World";
-      lblFirst.Location = new Point(150,200);
-      mainForm.Controls.Add(lblFirst);
-      Application.Run(mainForm);
+      Application.Run(new Gui());
     }
     private static void commandline(string[] args){
       RootCommand rootCommand = new RootCommand();
